e205a404bbf07eff9b860389a2190bc7766d6ed5
TABURISD <36299789+TABURISD@users.noreply.github.com>
2018-03-26 20:22:59 +0800

    Delete conv_1_2.h

--- source/conv_1_2.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONV_1_2_H_
-#define CONV_1_2_H_
-
-void encoder_init(); //编码前调用
-int encode(int input); //1bit输入 2bit输出
-void decoder_init(int initialstate);//解码前调用
-int decode(int input);//2bit输入 1bit输出
-
-#endif
